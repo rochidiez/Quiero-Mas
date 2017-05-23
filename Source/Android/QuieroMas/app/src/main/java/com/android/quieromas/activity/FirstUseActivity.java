8de7e47888aaf85680884551f4fcf4817675935d
lucas <lucasputerman@gmail.com>
2017-05-23 16:48:45 -0300

    fix for firebase function, changed +

--- Source/Android/QuieroMas/app/src/main/java/com/android/quieromas/activity/FirstUseActivity.java
+++ Source/Android/QuieroMas/app/src/main/java/com/android/quieromas/activity/FirstUseActivity.java
@@ -156,7 +156,7 @@ public class FirstUseActivity extends AuthActivity {
         // ?text=IBObsbDGMNRrC1ra5U50QpBhuyE3+lucas+10/5/1990+lucasputerman@gmail.com+10/5/2016+bebe+bebito
         StringBuilder builder = new StringBuilder();
         builder.append(uid);
-        builder.append("+");
+        builder.append(" ");
 
         String[] splittedName = name.split(" ");
         for(int i = 0; i < splittedName.length; i++){
@@ -165,13 +165,13 @@ public class FirstUseActivity extends AuthActivity {
                 builder.append("-");
             }
         }
-        builder.append("+");
+        builder.append(" ");
         builder.append(birthdate);
-        builder.append("+");
+        builder.append(" ");
         builder.append(email);
-        builder.append("+");
+        builder.append(" ");
         builder.append(babyBirthdate);
-        builder.append("+");
+        builder.append(" ");
         String[] splittedBabyName = babyName.split(" ");
         for(int i = 0; i < splittedBabyName.length; i++){
             builder.append(splittedBabyName[i]);
@@ -179,7 +179,7 @@ public class FirstUseActivity extends AuthActivity {
                 builder.append("-");
             }
         }
-        builder.append("+");
+        builder.append(" ");
         String[] splittedBabyNickname = babyNickname.split(" ");
         for(int i = 0; i < splittedBabyNickname.length; i++){
             builder.append(splittedBabyNickname[i]);
